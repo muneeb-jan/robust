06f16f5aa183294d0ecd4bf03a92f480939f66b0
Oleksii Oleksenko <alexo_o@ukr.net>
2020-05-19 16:11:29 +0200

    Lab migration

--- fputs_test.cpp
+++ fputs_test.cpp
@@ -2,8 +2,8 @@
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
 #include <string>
 #include <iostream>
@@ -12,33 +12,32 @@
 #include "tests.h"
 #include "stats.h"
 
-FILE* generateFILE (int test_id)
-{
-	// generate test FILE* test value
-	// see tests.h
-	// use the functions specified by tools.h to create appropriate test values
-	// you can use a copy of test.txt as file to test on
+FILE *generateFILE(int test_id) {
+    // generate test FILE* test value
+    // see tests.h
+    // use the functions specified by tools.h to create appropriate test values
+    // you can use a copy of test.txt as a file to test on
 }
 
-const char* generateCSTR (int test_id)
-{
-	// generate test const char* test value
-	// see tests.h
-	// use the functions specified by tools.h to create appropriate test values
+const char *generateCSTR(int test_id) {
+    // generate a `const char*` test value
+    // see tests.h
+    // use the functions specified by tools.h to create appropriate test values
 }
 
-const double wait_time = 0.01;   // wait time before quering childs exit status
+// waiting time before querying the child's exit status
+// You might want to try using a smaller value in order to get the CI results faster,
+// but there is a chance that your tests will start failing because of the timeout
+const double wait_time = 1.0;
 
-void test_fputs (const TestCase& str_testCase, const TestCase& file_testCase)
-{
-	// execute single test
-	// use function of stats.h to record all tests
+void test_fputs(const TestCase &str_testCase, const TestCase &file_testCase) {
+    // execute a single test
+    // use the functions in stats.h to record all tests
 }
 
-int main (int argc, const char** argv)
-{
-	// execute all tests and catch exceptions
-	
-	return 0;
+int main(int argc, const char **argv) {
+    // execute all tests and catch exceptions
+
+    return 0;
 }
 
--- stats.h
+++ stats.h
@@ -1,13 +1,13 @@
 /* 
- * File belongs to task 4 of SFT computer lab course ws05.
+ * The file belongs to task 4 of SFT Lab
  * No need to change.
  *
- * - specifies functions for statistic recording in a common way
- * - make sure that you use the functions as descripted
+ * - specifies functions for statistic recording in a unified way
+ * - make sure that you use the functions as described
  * - these function will be instrumented for automatic task checking
  * - wrong use may lead to failing the task
  */
- 
+
 #ifndef _STATS_H_
 #define _STATS_H_
 
@@ -15,47 +15,47 @@
 
 /**
  * must be called for each test before it is started.
- * prints the current test configuration out.
- * after each test, the corresponding record_*_test_fputs (..) function must
+ * prints the current test configuration.
+ * after each test, the corresponding `record_*_test_fputs (..)` function must
  * be called to record the test results
- * @param cstr_testCase specifies test value used for as string parameter for fputs
- * @param file_testCase specifies test value used for as stream parameter for fputs
+ * @param cstr_testCase specifies the test value used as a string parameter for fputs
+ * @param file_testCase specifies the test value used as a stream parameter for fputs
  */
-void record_start_test_fputs (const TestCase& cstr_testCase,
-	const TestCase& file_testCase);
-	
+void record_start_test_fputs(const TestCase &cstr_testCase,
+                             const TestCase &file_testCase);
+
 /**
  * must be called for each test where fputs has crashed
  * prints the the test result
  * @param signal signal number that causes the crash
  */
-void record_crashed_test_fputs (int signal);
+void record_crashed_test_fputs(int signal);
 
 /**
  * must be called for each test where fputs is stopped in a debugger
- * (very unlikly to happen, only for completness)
+ * (very unlikely to happen)
  * prints the the test result
  * @param signal signal number that causes the stop
  */
-void record_stopped_test_fputs (int signal);
+void record_stopped_test_fputs(int signal);
 
 /**
- * must be called for each test where fputs terminates normaly
+ * must be called for each test where fputs terminates normally
  * prints the the test result
- * @param returnval the returnvalue of the process
+ * @param returnval the return value of the process
  */
-void record_ok_test_fputs (int returnval);
+void record_ok_test_fputs(int returnval);
 
 /**
  * must be called for each test where fputs does not finish within the given timeout
  * prints the the test result
  */
-void record_timedout_test_fputs ();
+void record_timedout_test_fputs();
 
 /**
  * prints the overall statistics of the experiments
  * must be called after all test has been performed
  */
-void print_summary ();
+void print_summary();
 
 #endif
